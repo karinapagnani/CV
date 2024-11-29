c6a51b7f315a1ea64be6a235e95ff477388417df
karinapagnani <159541094+karinapagnani@users.noreply.github.com>
2024-11-29 11:19:45 -0300

    certinhooo

--- lojas/lojas.js
+++ lojas/lojas.js
@@ -1,3 +1,35 @@
+function filtrarPorCategoria(event) {
+   
+    event.preventDefault();
+
+    
+    const categoriaSelecionada = event.target.getAttribute('data-categoria');
+
+    
+    const produtos = document.querySelectorAll('.tudo .produtos1 div, .tudo .produtos2 div, .produtos3 div');
+
+    
+    produtos.forEach(produto => {
+       
+        const categoriaProduto = produto.getAttribute('data-categoria');
+
+        if (categoriaSelecionada === 'todas' || categoriaSelecionada === categoriaProduto) {
+          
+            produto.style.display = 'block';
+        } else {
+            
+            produto.style.display = 'none';
+        }
+    });
+}
+
+
+const linksCategorias = document.querySelectorAll('.filtros a, .filtros button');
+linksCategorias.forEach(link => {
+    link.addEventListener('click', filtrarPorCategoria);
+});
+
+
 
 const suggestions = {
     "Fone Bluetooth branco": "/produtos/Produto1/fone.html",
